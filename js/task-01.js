13fb72aaa3204d44be5a9bf17af0c970b7860778
Nataliia Matsko <mask.dancing11@gmail.com>
2023-02-18 09:35:59 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,12 @@
-const elem = document.getElementsByTagName('ul')[0].getElementsByTagName('li');
+const numberOfCategoriesList = document.querySelector('#categories').children.length
+console.log('Number of categories: ${numberOfCategoriesList}');
+const listEl = document.querySelectorAll('ul > .item');
+const titlesEl = document.querySelectorAll('.item > h2');
+const itemsEl = document.querySelectorAll('.item > ul');
+for (let i = 0; i < listEl.length; i += 1) {
+    const CategoryName = titlesEl[i].textContent;
+    const ElementsQuantity = itemsEl[i].children.length;
+
+    console.log('Category: ${CategoryName}');
+    console.log('Elements: ${ElementsQuantity}');
+}
