4050e02725d702bec64473a0f86315923c193ecb
Nataliia Matsko <mask.dancing11@gmail.com>
2023-02-20 10:03:21 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,27 @@
 const formEl = document.querySelector('.login-form');
-formEl.addEventListener('submit', onFormSubmit);
+
+formEl.addEventListener('submit', onFormSubmit); 
+
 function onFormSubmit(event) {
     event.preventDefault();
+
     const formElements = event.currentTarget.elements;
     const email = formElements.email.value;
     const password = formElements.password.value;
+
     if (formEl.email.value === '' || formEl.password.value === '') {
-        alert('All fields have to be filled!');
+        alert('All fields has to be filled!');
     }
+    
     else {
+
         const formData = {
-            email,
+            email, 
             password,
         }
-        console.log('formData');
-        event.currentTarget.reset();
+        
+    console.log(formData);
+
+    event.currentTarget.reset();
     }
 };
